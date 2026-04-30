539faea25471f9b848ea5d94f8d838fd4f101659
Aayusha Chitrakar <chitrakaraayusha50@gmail.com>
2026-04-30 08:14:02 +0545

    User Entity class added

---
+++ Entities/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+namespace SkillBridge.Api.Entities{
+using System.Dynamic;
+
+public class User
+{
+    [Required,Key]
+public int Id {get; set;}
+[Required]
+public string name {get; set;}
+[Required]
+public string email{get; set;}
+[Required]
+public string PasswordHash{get; set;}
+[Required]
+public string Type{get; set;}
+[Required]
+public DateTime CreatedAt{get;set;}
+public bool IsActive {get; set;}
+}}
